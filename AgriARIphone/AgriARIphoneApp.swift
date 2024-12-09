27da20538a0e7543571f00238a9f33f705e020f2
lukasburkhardt <lukas.burkhardt@envidual.com>
2024-12-09 08:11:25 +0100

    Initial Commit

---
+++ AgriARIphone/AgriARIphoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgriARIphoneApp.swift
+//  AgriARIphone
+//
+//  Created by Lukas Burkhardt on 09.12.24.
+//
+
+import SwiftUI
+
+@main
+struct AgriARIphoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AgriARIphoneTests/AgriARIphoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  AgriARIphoneTests.swift
+//  AgriARIphoneTests
+//
+//  Created by Lukas Burkhardt on 09.12.24.
+//
+
+import Testing
+@testable import AgriARIphone
+
+struct AgriARIphoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
